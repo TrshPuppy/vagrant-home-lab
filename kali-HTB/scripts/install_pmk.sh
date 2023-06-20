fbc9da333e6432bcf5815aba10ddc64570111eea
trshpuppy <rosepineau@protonmail.com>
2023-06-20 13:41:24 -0400

    Add logic for if already  installed + fix comments.

--- kali-HTB/scripts/install_pmk.sh
+++ kali-HTB/scripts/install_pmk.sh
@@ -1,6 +1,6 @@
 #!/usr/zsh
 
-echo "                         ---------- STARTING INSTALL_PMK.SH ----------"
+echo "                         ---------- STARTING INSTALL_PMK.SH"
 cd /home/vagrant
 
 # Some useful globals:
@@ -8,15 +8,18 @@ user="hakcypuppy"
 user_home="/home/$user"
 
 # Pimp My Kali:
-echo "                                        installing pimp my kali..."
-	cd $user_home
-	rm -rf pimpmykali	
+echo "                                    ---- installing pimp my kali..."
+cd $user_home
+if [[ $(ls | grep "pimpmykali" -c) -eq 1 ]]; then
+	echo "                                    -- pmk already installed, skipping."
+else
+	echo  "                                   -- pmk not present, installing..."	
 	git clone https://github.com/TrshPuppy/pimpmykali-autopilot.git pimpmykali
-	cd $user_home/pimpmykali
-	
-	sudo ./pimpmykali.sh --auto
-#	sudo ./pimpmykali.sh --help
-
-echo "                                        Finished: pmk installed"
+	cd $user_home/pimpmykali	
+    #	sudo ./pimpmykali.sh --auto
+	sudo ./pimpmykali.sh --help
+fi
+echo "                                    ---- Finished: pmk installed"
 # Finishing up:
 cd /home/vagrant
+echo "                         ---------- Finished.
