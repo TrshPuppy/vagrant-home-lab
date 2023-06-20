46c7e939df7136f970944d755dc57da3f74180a3
trshpuppy <rosepineau@protonmail.com>
2023-06-20 11:17:22 -0400

    fix alignments/shell

--- kali-HTB/scripts/post_build_tests.sh
+++ kali-HTB/scripts/post_build_tests.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
+
+echo "                         ---------- RUNNING TESTS ----------"
 
 # This script should be run after all other provisioning scripts to ensure the other scripts were successful:
 cd /home/vagrant
-
-echo ":           ---------- ---------- ---------- RUNNING TESTS ---------- ---------- ----------           :"
 
 # Some globals:
 declare -i error_count=0
@@ -18,56 +18,57 @@ user_home_files=(".vimrc")
 installed_tools=("git" "vim")
 
 # Check for user and user directories:
-	echo "                                                Checking for user config"
+echo "                                        Checking for user config..."
 	target_user=$(sudo cat /etc/passwd | grep $user -c)
 
 	if [[ $target_user -eq 1 ]]; then
-		echo "                                                    user $user exists in /etc/passwd"
+		echo "                                            user $user exists in /etc/passwd"
 		user_exists=true
 	else
-		echo "                                                    ERROR: missing user $user in /etc/passwd"
+		echo "                                            ERROR: missing user $user in /etc/passwd"
 		user_exists=false
 		error_count+=1
 	fi
 
+echo "                                        Checking user is sudo..."
 	# Check that user is in sudo group:
 	if [[ "$user_exists" == "true" ]]; then
 		exists_in_sudoers=$(cat /etc/group | grep "sudo" | grep $user -c)
 
 		if [[ $exists_in_sudoers -eq 1 ]]; then
-			echo "                                                    user $user is part of sudo group"
+			echo "                                            user $user is part of sudo group"
 		else
-			echo "                                                    ERROR: user $user is not part of sudo group"
+			echo "                                            ERROR: user $user is not part of sudo group"
 			error_count+=1	
 		fi
 	fi
 
+echo "                                       Checking for config  files in user home..."
 	# Check for config files in user home dir:
 	if [[ "$user_exists" == "true" ]]; then
 		for f in $user_home_files; do
 			f_exists=$(ls -a $user_home | grep $f -c)
 
 			if [[ $f_exists -eq 1 ]]; then
-				echo "                                                    cofig file $f exists"
+				echo "                                           cofig file $f exists"
 				continue
 			else
-				echo "                                                    ERROR: file $f is not present in $user_home"
+				echo "                                           ERROR: file $f is not present in $user_home"
 				error_count+=1	
 			fi
 		done
 	fi
 
-# Check that tools were installed:
-	echo "                                                Checking that tools installed"
-	
+echo "                                      Checking that tools installed successfully..."
+# Check that tools were installed:	
 	for t in $installed_tools; do
 		t_exists=$(dpkg -s $t | grep "install ok installed" -c)
 
 		if [[ $t_exists -eq 1 ]]; then
-			echo "                                                    tool $t exists"
+			echo "                                          tool $t exists"
 			continue
 		else
-			echo "                                                    ERROR: tool $t was not installed"
+			echo "                                          ERROR: tool $t was not installed"
 			error_count+=1	
 		fi
 	done
@@ -75,17 +76,17 @@ installed_tools=("git" "vim")
 # Check vagrant home directory:
 	# eh.... mayber later
 
+echo "                                      Checking for deleted pass.txt"
 # Check that pass was deleted:
-	echo "                                                Checking for deleted pass.txt"
 	p_exists=$(ls /tmp/vagrant/configs | grep "pass.txt" -c)
 
 	if [[ $p_exists -eq 1 ]]; then
-		echo "                                                    ERROR:  pass.txt not deleted from /tmp/vagrant/configs"
+		echo "                                      ERROR:  pass.txt not deleted from /tmp/vagrant/configs"
 		error_count+=1
 	else
-		echo "                                                    pass.txt successfully deleted"
+echo "                                              pass.txt successfully deleted"
 	fi
 	
 # Finishing up:
 cd /home/vagrant
-echo ":           ---------- --------- ---------- Exiting Tests w/ $error_count Errors ---------- ---------- ----------           "
+echo "                        ---------- Exiting Tests w/ $error_count Errors ----------"
