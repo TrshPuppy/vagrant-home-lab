f7b36f636099bddf6db51ec40902689589da1372
trshpuppy <rosepineau@protonmail.com>
2023-06-20 11:04:08 -0400

    fixed alignment and shell.

--- kali-HTB/scripts/configure.sh
+++ kali-HTB/scripts/configure.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # Add banner so we can tell what's happening:
-echo "/ ******************************* STARTING CONFIGURE.SH ******************************* \\"
+echo "                        ---------- STARTING CONFIGURE.SH ----------"
 cd /home/vagrant
 
 # Some nice globals:
@@ -11,25 +11,26 @@ cd /home/vagrant
 	user_home="/home/$user"
 
 # Make a new user:i
-echo ":                                  Adding user                                    :"
+echo "                                       Adding user"
 	# Make sure the user doesn't exist:
 	if cat "/etc/passwd" | grep -c $user; then
-		echo "user $user already exists...skipping..."
+		echo "                                            user $user already exists...skipping..."
 	else
 		sudo useradd -m -p $user_pass -s /bin/bash $user
+		echo "                                            user $user created"	
 	fi
 
-echo ":                               Adding to sudo Group                              :"
+echo "                                       Adding to sudo group"
 	sudo usermod -a -G sudo $user
 
 # Something about delete password file
-echo ":                                Removing pass file                               :"
+echo "                                       Removing pass file"
 #	sudo rm /home/vagrant/configs/pass.txt
 	sudo rm /tmp/vagrant/configs/pass.txt
 
 # Load Vim config from /configs:
-echo ":                                 Configuring Vim                                 :"
-	echo ":                          adding files and dirs...                           :"
+echo "                                       Configuring Vim"
+echo "                                             adding files and dirs..."
 	cd $user_home
 	mkdir .vim
 	mkdir .vim/autoload
@@ -37,9 +38,11 @@ echo ":                                 Configuring Vim                         
 	mkdir .vim/colors
 	mkdir .vim/plugged
 
-	echo ":                           copying over .vimrc...                            :"
+echo "                                             adding vim configuration file..."
 #	sudo cp /home/vagrant/shared-configs/.vimrc $user_home/.vimrc
 	sudo cp /tmp/vagrant/shared-configs/.vimrc $user_home/.vimrc
+
+echo "                                       finished: vim installed and configured"
 
 # Finishing up:
 cd /home/vagrant
