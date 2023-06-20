e4a6674bdcbf235633f91f1475322461a6186df6
trshpuppy <rosepineau@protonmail.com>
2023-06-20 14:09:59 -0400

    improve logic

--- kali-HTB/scripts/install_tools.sh
+++ kali-HTB/scripts/install_tools.sh
@@ -4,9 +4,9 @@
 declare -i tools_installed=0
 
 # Add banner so we can tell what's happening:
-echo "                        --------- STARTING INSTALL_TOOLS.SH"
+echo "                        ---------- STARTING INSTALL_TOOLS.SH"
 cd /home/vagrant
 
-echo "                                  Finished: $tools_installed tools installed"
+echo "                        ---------- Finished: $tools_installed tools installed"
 # Finishing up:
 cd /home/vagrant
