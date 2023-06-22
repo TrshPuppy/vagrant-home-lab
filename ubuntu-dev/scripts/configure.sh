2723aa15cc2e2e7218910d2bb8ff0818e69d8e08
trshpuppy <rosepineau@protonmail.com>
2023-06-22 17:49:11 -0400

    fix configure script

--- ubuntu-dev/scripts/configure.sh
+++ ubuntu-dev/scripts/configure.sh
@@ -1,22 +1,75 @@
-#!/bin/bash
+#!/bin/zsh
+
+# Some nice globals:
+	user="devypuppy"
+	user_pass=$(cat /tmp/vagrant/configs/pass.txt | mkpasswd --stdin)
+	user_home="/home/$user"
 
 # Banner:
 echo "                        -------- STARTING CONFIGURE.SH"
+cd /home/vagrant
 
-# Some globals:
-VM_CONFIGS_DIR="/home/vagrant/configs"
-VM_SHARED_CONFIGS_DIR="/home/vagrant/shared-configs"
+# Make a new user:
+echo "                                 ---- adding user..."
+# Make sure the user doesn't exist:
+if cat "/etc/passwd" | grep -c $user; then
+	echo "                                      -- user $user already exists...skipping..."
+else
+	sudo useradd -m -p $user_pass -s /bin/bash $user
+	echo "                                      -- user $user created"	
+fi
 
-# Load Vim config from /configs
-echo "                                 ---- creating vim env..."
-	cd ~
+# Add user to sudo group:
+echo "                                 ---- adding to sudo group..."
+sudo usermod -a -G sudo $user
+
+# Remove password file:
+	echo "                                      -- removing pass file..."
+	sudo rm /tmp/vagrant/configs/pass.txt
+echo "                                 ---- finished: new user added"
+
+# Load Vim config from /configs:
+echo "                                 ---- configuring vim..."
+# Check if already present:
+if [[ $(ls /home/$user -a | grep ".vimrc" -c) -eq 1 ]]; then
+	echo "                                      -- vim already configured, skipping,.."
+else
+	echo "                                      -- adding files and dirs..."
+	cd $user_home
 	mkdir .vim
 	mkdir .vim/autoload
 	mkdir .vim/backup
 	mkdir .vim/colors
 	mkdir .vim/plugged
 
-echo "                                 ---- copying over .vimrc..."
-	sudo cp $VM_SHARED_CONFIGS_DIR/.vimrc /home/vagrant/.vimrc
+	echo "                                      -- adding vim configuration file..."
+	sudo cp /tmp/vagrant/shared-configs/.vimrc $user_home/.vimrc
+fi
+echo "                                 ---- finished: vim installed and configured"
 
+# Finishing up:
+cd /home/vagrant
 echo "                        -------- FINISHED."
+
+# #!/bin/bash
+
+# # Banner:
+# echo "                        -------- STARTING CONFIGURE.SH"
+
+# # Some globals:
+# VM_CONFIGS_DIR="/home/vagrant/configs"
+# VM_SHARED_CONFIGS_DIR="/tmp/vagrant/shared-configs"
+
+# # Load Vim config from /configs
+# echo "                                 ---- creating vim env..."
+# 	cd ~
+# 	mkdir .vim
+# 	mkdir .vim/autoload
+# 	mkdir .vim/backup
+# 	mkdir .vim/colors
+# 	mkdir .vim/plugged
+
+# echo "                                 ---- copying over .vimrc..."
+# 	sudo cp $VM_SHARED_CONFIGS_DIR/.vimrc /home/vagrant/.vimrc
+
+# echo "                        -------- FINISHED."
