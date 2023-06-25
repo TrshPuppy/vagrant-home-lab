bd12055acf7d95dc47f5eedc35a0b64e2280a643
Rose <trshpuppy@proton.me>
2023-06-25 15:24:33 -0400

    Add some config things.

--- shared-scripts/install_golang.sh
+++ shared-scripts/install_golang.sh
@@ -1,67 +1,69 @@
 #!/bin/bash
 
-# Banner:
-echo "                  -------- STARTING INSTALL_GOLANG.SH"
+echo "golang"
 
-# Make sure we're in the right place:
-#       Also this is probably not secure...
-target_user=$1
-cd "/home/$target_user"
-source .profile
+# # Banner:
+# echo "                  -------- STARTING INSTALL_GOLANG.SH"
 
-# Some globals:
-declare -i go_present=$(go version 2>/dev/null | grep -c 'go')
-version_req=$(curl -sSL "https://golang.org/dl/?mode=json")
-current_go_version=$(echo "$version_req" | grep "go" | cut -d "\"" -f 4 | head -n 1)
+# # Make sure we're in the right place:
+# #       Also this is probably not secure...
+# target_user=$1
+# cd "/home/$target_user"
+# source .profile
 
-install_go(){
-    # Install using current version:
-    echo "                           ---- fetching tarball..."
-    wget "https://golang.org/dl/$current_go_version.linux-amd64.tar.gz" >/dev/null
-}
+# # Some globals:
+# declare -i go_present=$(go version 2>/dev/null | grep -c 'go')
+# version_req=$(curl -sSL "https://golang.org/dl/?mode=json")
+# current_go_version=$(echo "$version_req" | grep "go" | cut -d "\"" -f 4 | head -n 1)
 
-extract_tarball(){
-    echo "                           ---- extracting tarball..."
-    sudo rm -rf /usr/local/go && \
-    tar -C /usr/local -xzf "$current_go_version.linux-amd64.tar.gz"
-}
+# install_go(){
+#     # Install using current version:
+#     echo "                           ---- fetching tarball..."
+#     wget "https://golang.org/dl/$current_go_version.linux-amd64.tar.gz" >/dev/null
+# }
 
-update_environment(){
-    echo "                           ---- updating environment..."
-    echo "PATH=\$PATH:/usr/local/go/bin" >> "/home/$target_user/.profile"
+# extract_tarball(){
+#     echo "                           ---- extracting tarball..."
+#     sudo rm -rf /usr/local/go && \
+#     tar -C /usr/local -xzf "$current_go_version.linux-amd64.tar.gz"
+# }
 
-    cd /home/$target_user
-    echo "                                -- sourcing .profile..."
-    source .profile
-}
+# update_environment(){
+#     echo "                           ---- updating environment..."
+#     echo "PATH=\$PATH:/usr/local/go/bin" >> "/home/$target_user/.profile"
 
-clean_up(){
-    echo "                           ---- cleaning up..."
-    rm "$current_go_version.linux-amd64.tar.gz"
-}
+#     cd /home/$target_user
+#     echo "                                -- sourcing .profile..."
+#     source .profile
+# }
 
-verify_install(){
-    echo "                           ---- verifying installation..."
+# clean_up(){
+#     echo "                           ---- cleaning up..."
+#     rm "$current_go_version.linux-amd64.tar.gz"
+# }
 
-    declare -i post_install=$(go version 2>/dev/null | grep -c 'go')
-    if [[ $post_install -eq 1 ]]; then 
-        echo "                                -- go successfully installed."
-    else
-        echo "                                -- go failed to install."
-    fi
-}
+# verify_install(){
+#     echo "                           ---- verifying installation..."
 
-# Start
-echo "                           ---- checking for go..."
-if [[ $go_present -eq 0 ]]; then
-     echo "                                -- go not installed. Continuing..."
-     install_go
-     extract_tarball
-     update_environment 
-     clean_up 
-     verify_install
-else
-    echo "                                -- go already installed. Skipping..."
-fi
+#     declare -i post_install=$(go version 2>/dev/null | grep -c 'go')
+#     if [[ $post_install -eq 1 ]]; then 
+#         echo "                                -- go successfully installed."
+#     else
+#         echo "                                -- go failed to install."
+#     fi
+# }
 
-echo "                  -------- FINISHED."
+# # Start
+# echo "                           ---- checking for go..."
+# if [[ $go_present -eq 0 ]]; then
+#      echo "                                -- go not installed. Continuing..."
+#      install_go
+#      extract_tarball
+#      update_environment 
+#      clean_up 
+#      verify_install
+# else
+#     echo "                                -- go already installed. Skipping..."
+# fi
+
+# echo "                  -------- FINISHED."
