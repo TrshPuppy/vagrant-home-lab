ad560f66eb9acf9fba07b359d186d1af53bb627d
Rose <trshpuppy@proton.me>
2023-07-30 10:27:20 -0400

    fix maintenance script, add to end of Vagrantfile

--- shared-scripts/maintenance.sh
+++ shared-scripts/maintenance.sh
@@ -6,18 +6,23 @@ cd /home/vagrant
 
 # Autoremove in case we left some dumb shiznit behind:
 echo "          ---- purging unkempt packages..."
-sudo apt autoremove --purge &>/dev/null
-echo "          ---- finished: apt packages purged."
+sudo apt autoremove --purge &>/dev/null \
+&& echo "               -- finished: apt packages purged."
 
 # Update and Upgrade:
-echo "          ---- updating apt packages..."
-sudo apt update && sudo apt upgrade -y &>/dev/null
-echo "          ---- finished: apt packages updated/ upgraded."
+echo "          ---- updating/ upgrading..."
+echo "               -- updating apt packages..."
+sudo apt update &>/dev/null \
+&& echo "               -- upgrading apt packages..." \
+&& sudo apt upgrade -y &>/dev/null \
+&& echo "       ---- finished: apt packages updated/ upgraded."
 
 # Clean up potentially interupted packages:
-sudo dpkg --configure -a &>/dev/null
+echo "          ---- making sure packages finished installing..."
+echo "               -- running dpkg configure..."
+sudo dpkg --configure -a &>/dev/null \
+&& echo "          ---- finished install check..."
 
 # Finishing up:
 cd /home/vagrant
 echo " -------- FINISHED: apt package maintenance complete."
-
