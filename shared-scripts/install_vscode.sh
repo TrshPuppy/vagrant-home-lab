2ffa64fe3b15761e97bd8f387b72c0b2503fe3d0
Rose <trshpuppy@proton.me>
2023-07-30 07:12:41 -0400

    fix vscode install

--- shared-scripts/install_vscode.sh
+++ shared-scripts/install_vscode.sh
@@ -7,9 +7,9 @@ echo "                           STARTING INSTALL_VSCODE.SH <------- "
 
 # Make sure we're in the right place/ context:
 configs_path="/tmp/vagrant/configs"
-user=$(cat $configs_path/box_env.txt | grep "user" | cut -d ":" -f 2 | tr -d '\r')
-shell=$(cat $configs_path/box_env.txt | grep "shell" | cut -d ":" -f 2 | tr -d '\r')
-cd /home/$user
+# user=$(cat $configs_path/box_env.txt | grep "user" | cut -d ":" -f 2 | tr -d '\r')
+# shell=$(cat $configs_path/box_env.txt | grep "shell" | cut -d ":" -f 2 | tr -d '\r')
+cd /home/vagrant
 source .profile
 
 # Some globals:
@@ -29,13 +29,13 @@ handle_vs_code(){
     echo "                         installing vscode... --"
     # From this resource: https://code.visualstudio.com/docs/setup/linux#_installation
     sudo apt-get install wget gpg
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    rm -f packages.microsoft.gpg
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg \
+    && sudo install -D -o vagrant -g vagrant -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg \
+    && sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list' \
+    && rm -f packages.microsoft.gpg
 
-    sudo apt update 2>/dev/null
-    sudo apt install code 2>/dev/null
+    sudo apt update 2>/dev/null \
+    && sudo apt install code 2>/dev/null
 }
 
 echo "                          checking for vscode... ----"
